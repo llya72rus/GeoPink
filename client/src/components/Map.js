64a7c7ed6f9004aa8467f5bf6fd84ca6764655c1
ilya72rus <makiliv72@gmail.com>
2019-04-09 08:41:33 +0300

    added draft pink onClick

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,88 +1,115 @@
-import React, {useState, useEffect} from "react";
-import ReactMapGL, {NavigationControl, Marker} from 'react-map-gl'
-import { withStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
-import PinIcon from '../components/PinIcon'
+import React, { useState, useEffect, useContext } from 'react';
+import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl';
+import { withStyles } from '@material-ui/core/styles';
+import Context from '../context';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/DeleteTwoTone';
+import PinIcon from '../components/PinIcon';
 
 const INITIAL_VIEPORT = {
   latitude: 37.7577,
   longitude: -122.4376,
   zoom: 13
-}
+};
 
 const Map = ({ classes }) => {
   const [viewport, setViewport] = useState(INITIAL_VIEPORT);
+  const { state, dispatch } = useContext(Context);
   const [userPosition, setUserPosition] = useState(null);
   useEffect(() => {
-    getUserPosition()
-  }, [])
+    getUserPosition();
+  }, []);
+
   const getUserPosition = () => {
-    if('geolocation' in navigator) {
+    if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords;
         setViewport({ ...viewport, latitude, longitude });
-        setUserPosition({ latitude, longitude })
+        setUserPosition({ latitude, longitude });
       });
     }
-  }
+  };
+
+  const handleMapClick = ({ lngLat, leftButton }) => {
+    if (!leftButton) return;
+    if (!state.draft) {
+      dispatch({ type: 'CREATE_DRAFT' });
+    }
+    const [longitude, latitude] = lngLat;
+    dispatch({
+      type: 'UPDATE_DRAFT_LOCATION',
+      payload: { longitude, latitude }
+    });
+  };
+
   return (
-  <div className={classes.root}>
-    <ReactMapGL 
-      width='100vw'
-      height='calc(100vh - 64px)'
-      mapStyle='mapbox://styles/mapbox/streets-v9'
-      mapboxApiAccessToken='pk.eyJ1IjoibW9obmJsdW1lNzIiLCJhIjoiY2p1N3c2djh4MDNqeTQ5dGZ3a3J6MWphMiJ9.Fc5KMLNYpsIvCst5QcqI7g'
-      {...viewport}
-      onViewportChange={(newViewport) => setViewport(newViewport)}
-    >
-    <div className={classes.navigationControl}>
-      <NavigationControl onViewportChange={(newViewport) => setViewport(newViewport)} />
+    <div className={classes.root}>
+      <ReactMapGL
+        width='100vw'
+        height='calc(100vh - 64px)'
+        mapStyle='mapbox://styles/mapbox/streets-v9'
+        mapboxApiAccessToken='pk.eyJ1IjoibW9obmJsdW1lNzIiLCJhIjoiY2p1N3c2djh4MDNqeTQ5dGZ3a3J6MWphMiJ9.Fc5KMLNYpsIvCst5QcqI7g'
+        onClick={handleMapClick}
+        {...viewport}
+        onViewportChange={(newViewport) => setViewport(newViewport)}>
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={(newViewport) => setViewport(newViewport)}
+          />
+        </div>
+        {userPosition && (
+          <Marker
+            latitude={userPosition.latitude}
+            longitude={userPosition.longitude}
+            offsetLeft={-19}
+            offsetTop={-38}>
+            <PinIcon size={40} color='red' />
+          </Marker>
+        )}
+
+        {state.draft && (
+          <Marker
+            latitude={state.draft.latitude}
+            longitude={state.draft.longitude}
+            offsetLeft={-19}
+            offsetTop={-38}>
+            <PinIcon size={40} color='hotpink' />
+          </Marker>
+        )}
+      </ReactMapGL>
     </div>
-    {userPosition && (
-      <Marker 
-        latitude={userPosition.latitude} 
-        longitude={userPosition.longitude}
-        offsetLeft={-19}
-        offsetTop={-38}
-      >
-        <PinIcon size={40} color='red' />
-      </Marker>
-    )}
-    </ReactMapGL>
-  </div>  
   );
 };
 
 const styles = {
   root: {
-    display: "flex"
+    display: 'flex'
   },
   rootMobile: {
-    display: "flex",
-    flexDirection: "column-reverse"
+    display: 'flex',
+    flexDirection: 'column-reverse'
   },
   navigationControl: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
-    margin: "1em"
+    margin: '1em'
   },
   deleteIcon: {
-    color: "red"
+    color: 'red'
   },
   popupImage: {
-    padding: "0.4em",
+    padding: '0.4em',
     height: 200,
     width: 200,
-    objectFit: "cover"
+    objectFit: 'cover'
   },
   popupTab: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column"
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
   }
 };
 
--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,25 +1,43 @@
 export default (state, action) => {
-   switch (action.type) {
-     case 'LOGIN_USER':
-       return {
-         ...state,
-          currentUser: action.payload
-       };
+  switch (action.type) {
+    case 'LOGIN_USER':
+      return {
+        ...state,
+        currentUser: action.payload
+      };
 
-      case 'IS_LOGGED_IN':
-       return {
-         ...state,
-         isAuth: action.payload
-       };
+    case 'IS_LOGGED_IN':
+      return {
+        ...state,
+        isAuth: action.payload
+      };
 
-       case 'SIGNOUT_USER':
-       return {
-         ...state,
-         currentUser: null,
-         isAuth: false
-       };
-   
-     default:
-       return state;
-   }
-}
+    case 'SIGNOUT_USER':
+      return {
+        ...state,
+        currentUser: null,
+        isAuth: false
+      };
+
+    case 'CREATE_DRAFT':
+      return {
+        ...state,
+        draft: {
+          latitude: 0,
+          longitude: 0
+        }
+      };
+
+    case 'UPDATE_DRAFT_LOCATION':
+      return {
+        ...state,
+        draft: {
+          latitude: action.payload.latitude,
+          longitude: action.payload.longitude
+        }
+      };
+
+    default:
+      return state;
+  }
+};
